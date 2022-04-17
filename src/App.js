c066e16d0bc73ef6a51e54d3908f00db7b161d73
Luciano Carpignano <lcarpignano@gmail.com>
2022-04-17 17:18:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
+import imgRickMorty from '../src/img/rick-morty.png';
+import Characters from './components/Characters';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const handleOnClickSearchCharacters = async () => {
+    //TODO add some logic to get random different character each search
+    // characters count 826
+    const randomArr = [];
+    while (randomArr.length < 12) {
+      randomArr.push(Math.floor(Math.random() * 286));
+    }
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${randomArr}`);
+    const data = await response.json();
+
+    console.log(randomArr);
+    console.log(data);
+
+    setCharacters(data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title" >Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imgRickMorty} alt="Rick & Morty" className="img-home" />
+            <button
+              onClick={handleOnClickSearchCharacters}
+              className="btn-search"
+            >
+              Random Characters
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
---
+++ src/components/Characters.js
@@ -0,0 +1,49 @@
+export default function Characters({ characters, setCharacters }) {
+    console.log(characters);
+
+    const handlerOnClickBackHome = () => {
+        setCharacters(null);
+    }
+
+    return (
+        <div className="characters">
+            <h1>Characters</h1>
+            <span className="back-home" onClick={handlerOnClickBackHome} >Back home...</span>
+            <div className="container-characters">
+                {characters?.map((character, index) => (
+                    <div className="character-container" key={index} >
+                        <div>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive" ? (
+                                    <>
+                                        <span className="alive" />
+                                        Alive
+                                    </>
+                                ) : (
+                                    <>
+                                        <span className="dead" />
+                                        Dead
+                                    </>
+                                )}
+                            </h6>
+                            <p >
+                                <span className="text-grey">Episodes: </span>
+                                <span> {character.episode.length} </span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Species: </span>
+                                <span> {character.species} </span>
+                            </p>
+                        </div>
+                    </div>
+
+                ))};
+            </div>
+            <span className="back-home" onClick={handlerOnClickBackHome} >Back home...</span>
+        </div>
+    )
+}
